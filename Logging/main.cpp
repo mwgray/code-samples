3794d1f79deb4e0d91f9950abb490364ebe0b74b
Matthew Gray <mwgray@gmail.com>
2016-08-05 17:48:28 -0700

    sample

--- Logging/main.cpp
+++ Logging/main.cpp
@@ -4,11 +4,11 @@
 
 using namespace core;
 
-void LogSession() {
-    sinfo("Physics", "Aligning %d boson particles", 42);
-    strace("Physics", "Aligning boson particles 0");
-    strace("Physics", "Aligning boson particles 1");
-    strace("Physics", "Aligning boson particles 3");
+void LogSession(int count) {
+    sinfo("Physics", "Aligning %d boson particles", count);
+    for (int i = 0; i < count; ++i) {
+        strace("Physics", "Aligning boson particles %d", i);
+    }
 }
 
 int main() {
@@ -16,12 +16,12 @@ int main() {
     sforce("Example", "By default, the logger is not subscribed to any categories");
     sforce("Example", "But force logs are always shown");
 
-    LogSession();
+    LogSession(4);
 
     sforce("Example", "Lets subscribe to physics");
     SimpleLogger::instance.SubscribeTo("Physics");
 
-    LogSession();
+    LogSession(4);
 
     return 0;
 }
