2f2ff40db44b85588847598bf458673856443083
Matthew Gray <mwgray@gmail.com>
2016-08-07 21:58:59 -0700

    stripped down version of word cache

--- WordCache/WordCachePch.h
+++ WordCache/WordCachePch.h
@@ -16,23 +16,4 @@
 typedef unsigned int U32;
 typedef unsigned short U16;
 
-namespace Phyre {
-
-    typedef char PChar;
-    typedef unsigned int PUInt32;
-    typedef unsigned char PUInt8;
-    typedef unsigned short PUInt16;
-
-    template<typename T>
-    class PArray
-    {
-    public:
-        inline const T &operator [] (PUInt32 index) const
-        {
-            T t;
-            return t;
-        }
-    };
-}
-
 #endif //__WORDCACHEPCH_H__
